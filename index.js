144732a20aa2655256520a3a757e45e67f0a9e2d
Sam Foster <sfoster@sneetchworks.com>
2013-03-13 22:39:07 +0000

    Now using st for the static file serving and directory indexes.

--- index.js
+++ index.js
@@ -1,53 +1,55 @@
-var connect = require('connect'), 
-	express = require('express'), 
-	cwd = process.cwd(), 
-    argv = require('optimist').argv, 
-	docRoot = cwd;
+var st = require('st'),
+    http = require('http'),
+    cwd = process.cwd(),
+    argv = require('optimist').argv,
+    docRoot = cwd;
 
 // delegation the way I like it
 var delegate = function(proto, mixin){
-	var o = Object.create(proto);
-	if(mixin){
-		for(var i in mixin) o[i] = mixin[i];
-	}
-	return o;
+  var o = Object.create(proto);
+  if(mixin){
+    for(var i in mixin) o[i] = mixin[i];
+  }
+  return o;
 };
 
-var server = express.createServer();
-
-//
 // set up config for the server,
 //      overriding defaults from argv
 //
 var configDefaults = {
-	docRoot : docRoot,
-	port: 3000, 
-	hostname: 'localhost',
-	verbose: false,
-	logger: 'short'
+  docRoot : docRoot,
+  port: 3000,
+  hostname: 'localhost',
+  verbose: false,
+  logger: 'short'
 };
 var appConfig = delegate(configDefaults, argv);
-
 var verbose = appConfig.verbose;
 
-server.configure(function(){
-	if(appConfig.logger){
-	    server.use(express.logger(appConfig.logger));
-	}
-    server.use(express['static'](appConfig.docRoot));
-	server.use(express.directory(appConfig.docRoot));
-});
-
 if(require.main === module){
-	if("help" in argv){
-		var helpStr = "Usage: node index.js [options]\n\n";
-			helpStr += "Options:\n";
-		Object.keys(configDefaults).forEach(function(name){
-			helpStr += "  --"+name+ " "+ configDefaults[name] + "\n";
-		});
-		console.log(helpStr);
-	} else {
-		server.listen(appConfig.port, appConfig.hostname);
-	}
-	console.log('serving on %s:%s; out of %', appConfig.hostname, appConfig.port, appConfig.docRoot);
+  if("help" in argv){
+    var helpStr = "Usage: node index.js [options]\n\n";
+      helpStr += "Options:\n";
+    Object.keys(configDefaults).forEach(function(name){
+      helpStr += "  --"+name+ " "+ configDefaults[name] + "\n";
+    });
+    console.log(helpStr);
+  } else {
+
+    var mount = st({
+      path: appConfig.docRoot,
+      url: '/',
+      index: true,
+      dot: false,
+    });
+
+    http.createServer(function(req, res){
+      if( mount(req, res)) return;
+      if(appConfig.verbose) {
+        console.log("Unable to handle request for " + req.url);
+      }
+    }).listen(appConfig.port, appConfig.hostname);
+
+  }
+  console.log('serving on %s:%s; out of %', appConfig.hostname, appConfig.port, appConfig.docRoot);
 }
